6d9bc0e566a412cbe36a8d4a704691b166be5970
Mint <skylove.exe@gmail.com>
2021-05-02 12:58:56 +0900

    Initial Commit

---
+++ NotificationTest/NotificationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationTestApp.swift
+//  NotificationTest
+//
+//  Created by hato on 2021/05/02.
+//
+
+import SwiftUI
+
+@main
+struct NotificationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
