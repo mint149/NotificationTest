2649e30e766ae070fead6c00b0998e4451e5ac12
Mint <skylove.exe@gmail.com>
2021-05-02 15:35:58 +0900

    爆破時刻を設定可能にした

--- NotificationTest/ContentView.swift
+++ NotificationTest/ContentView.swift
@@ -8,45 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectionDate = Date()
     var body: some View {
-        Button(action: {
-            // 日付フォーマット
-            let date = Date()
-            let dateFormatter = DateFormatter()
-            dateFormatter.timeStyle = .medium
-            dateFormatter.dateStyle = .medium
-            dateFormatter.locale = Locale(identifier: "ja_JP")
-     
-            // 現在時刻の1分後に設定
-            let date2 = Date(timeInterval: 60, since: date)
-            let targetDate = Calendar.current.dateComponents(
-                [.year, .month, .day, .hour, .minute],
-                from: date2)
-     
-            let dateString = dateFormatter.string(from: date2)
-            print(dateString)
-     
-            // トリガーの作成
-            let trigger = UNCalendarNotificationTrigger.init(dateMatching: targetDate, repeats: false)
-     
-            // 通知コンテンツの作成
-            let content = UNMutableNotificationContent()
-            content.title = "iPhoneが爆発しました。"
-            content.body = ""
-            content.sound = UNNotificationSound.init(named: UNNotificationSoundName(rawValue: "explosion.mp3"))
-
-            // 通知リクエストの作成
-            let request = UNNotificationRequest.init(
+        VStack{
+            DatePicker("", selection: $selectionDate)
+                .labelsHidden()
+                .datePickerStyle(GraphicalDatePickerStyle())
+            Button(action: {
+                // 日付フォーマット
+                let dateFormatter = DateFormatter()
+                dateFormatter.timeStyle = .medium
+                dateFormatter.dateStyle = .medium
+                dateFormatter.locale = Locale(identifier: "ja_JP")
+                
+                let targetDate = Calendar.current.dateComponents(
+                    [.year, .month, .day, .hour, .minute],
+                    from: selectionDate)
+                
+                let dateString = dateFormatter.string(from: selectionDate)
+                print(dateString)
+                
+                // トリガーの作成
+                let trigger = UNCalendarNotificationTrigger.init(dateMatching: targetDate, repeats: false)
+                
+                // 通知コンテンツの作成
+                let content = UNMutableNotificationContent()
+                content.title = "iPhoneが爆発しました。"
+                content.body = ""
+                content.sound = UNNotificationSound.init(named: UNNotificationSoundName(rawValue: "explosion.mp3"))
+                
+                // 通知リクエストの作成
+                let request = UNNotificationRequest.init(
                     identifier: "CalendarNotification",
                     content: content,
                     trigger: trigger)
-            
-            // 通知リクエストの登録
-            let center = UNUserNotificationCenter.current()
-            center.add(request)
-            
-        }) {
-            Text("セットする")
+                
+                // 通知リクエストの登録
+                let center = UNUserNotificationCenter.current()
+                center.add(request)
+                
+            }) {
+                Text("セットする")
+            }
         }
     }
 }
